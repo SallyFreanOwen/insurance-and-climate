7b943538c779fec0730bac39a4eaa57bda777f14
Sally Owen <sally.owen@motu.org.nz>
2018-11-08 14:35:56 +1300

    added original netcdf data transform

--- main-data-process-to-histogram.R
+++ main-data-process-to-histogram.R
@@ -2,24 +2,29 @@
 
 ## Using sf package:
 
-#0 - for new mines (installing packages) 
-#source("EQC-0.R")
+#0 - for new machines (installing packages) 
+source("EQC-0.R")
+# Also at this point, add the Data to a folder called Data in the project. #NB note data names etc. 
 
 # 1 - eqc claims csv to r dataframe (including lossdate as an r date) 
 source("EQC-1-claims-import.R")
+
 # 2 - eqc portfolio csv to sf r data 
 source("EQC-2-portfolio-import.R")
+
 # 3 - vis portfolios 
 source("EQC-3-portfolio-vis.R")
+
 # 4 - vis claim locations 
 source("EQC-4-claim-vis.R")
+
 # 5 - niwa rain netcdf to r data (including day as r dates)
+#source("EQC-5a-NIWA-rain-import.R") # necessary if working from raw rain-data netcdf files, otherwise progress to 5b
 source("EQC-5-rain-import.R") 
+
 # 6 - vis rain 
 source("EQC-6-rain-vis.R")
-
-# 6b - optional (slight tangent) 
-## source("EQC-6b-Map-EQC-NIWA-points.R") # NB Step 6 makes R crash on my work computer 
+## source("EQC-6b-Map-EQC-NIWA-points.R") # Mapped instead of plotted - NB Step 6b makes R crash on my work computer 
 
 # 7 - geo process portfolio adding closest rain point 
 source("EQC-7-joining-grid-portfolio.R")
