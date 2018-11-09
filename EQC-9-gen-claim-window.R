c8d5d108a5479f8568a9422b26d2922b9ae0fd29
Sally Owen <sally.owen@motu.org.nz>
2018-11-09 15:55:09 +1300

    drafted plan - step by step

--- EQC-9-gen-claim-window.R
+++ EQC-9-gen-claim-window.R
@@ -1,4 +1,15 @@
 # Step 9 
 
-# Generate "claim window"
+### Generate "claim window"
+
+# claim$claimID->claim$claimLossDate->claim$PortfolioID->portfolio$portfoliolocation->precip$gridlocation->precip$rainfromday=LossDate-10today=LossDate+10 
+
+# The plan: working backwards... 
+
+# isolate grids that actually contain claims 
+# for each of those gridIDs, attach necessary claim LossDates 
+# claim windows: keep only grid-day combos that fall within claim windows 
+
+# Output goal: dataframe below
+# | claim ID | grid lat | grid lon | Lossdate | Precip t-10 through t+10 | 
 
