151e65ef103cb10cb5d82fffd5f513fabbbe3e9c
SallyFreanOwen <sally.frean@gmail.com>
2018-11-25 21:35:56 +0000

    WIP

--- EQC-8-gen-claim-window.R
+++ EQC-8-gen-claim-window.R
@@ -7,13 +7,20 @@
 
 # suppressing extra data?
 
-
 claimPortfolioGrid <- merge(claimWorking, portfolioNearestGrid, by = "PortfolioID")
 
 # Adding geometry column to rain
-precipSP <- st_as_sf(precipWorking, coords = c("longitude", "latitude"), crs = 4326)
+#precipSP <- st_as_sf(precipWorking, coords = c("longitude", "latitude"), crs = 4326)
+#claimPortfolioGridRain <- merge(claimPortfolioGrid, precipSP, by.x = "niwa_latlon", by.y = "geometry")
 
-claimPortfolioGridRain <- merge(claimPortfolioGrid, precipSP, by.x = "niwa_latlon", by.y = "geometry")
+# Splitting out the lat longs 
+portfolioNearestGrid$niwa_lon <- st_coordinates(portfolioNearestGrid$niwa_latlon)[,1]
+portfolioNearestGrid$niwa_lat <- st_coordinates(portfolioNearestGrid$niwa_latlon)[,2]
+#note rounded to 3dp
+
+portfolioGridRain <- merge(portfolioNearestGrid, precipWorking, by.x = c("niwa_lon", "niwa_lat"), by.y = c("longitude", "latitude"), allow.cartesian = TRUE)
+
+claimPortfolioGridWorking8 <- c(claimPortfolioGrid$claimID, claimPortfolioGrid$LossDate, claimPortfolioGrid$PortfolioID, claimPortfolioGrid$WGS84Longitude, claimPortfolioGrid$WGS84Latitude)
 
 # goal 2: 
 # add column "offset_day" from -10 to 10 which is niwaday-lossdate 
