4fa57c398556b9779237db7e0e3120a9a5bf2792
SallyFreanOwen <sally.frean@gmail.com>
2018-11-25 07:18:07 +0000

    working on claim window

--- EQC-8-gen-claim-window.R
+++ EQC-8-gen-claim-window.R
@@ -5,10 +5,18 @@
 # Output goal 1: dataframe below
 # | claim ID | LossDate | portfolioID | portfolio_latlon | niwa_latlon | niwa_day | rain | 
 
+# suppressing extra data?
+
 claimPortfolioGrid <- merge(claimWorking, portfolioNearestGrid, by = "PortfolioID")
+
+# Adding geometry column to rain
+precipSP <- st_as_sf(precipWorking, coords = c("longitude", "latitude"), crs = 4326)
+
+claimPortfolioGridRain <- merge(claimPortfolioGrid, precipSP, by.x = "niwa_latlon", by.y = "geometry")
+
 # goal 2: 
 # add column "offset_day" from -10 to 10 which is niwaday-lossdate 
-# add t-10 to t+10 rain? 
+# add t-10 to t+10 rain
 
 
 
