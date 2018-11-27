968790d1929a9156356d5087eeeea5b2c33d2fd0
SallyFreanOwen <sally.frean@gmail.com>
2018-11-27 15:09:37 +1300

    archiving older workflow

--- EQC-4-claim-vis.R
+++
@@ -1,6 +0,0 @@
-# EQC - 4 visualise only portfolios that have made claims 
-
-claimWorking <- merge(claimWorking, portfolioWorking, by = "PortfolioID") 
-claimSP <- st_as_sf(claimWorking, coords = c("WGS84Longitude", "WGS84Latitude"), crs = 4326)
-
-plot(st_geometry(claimSP), main = "Portfolios that have made EQC claims", pch = 1, cex = 0.1, col = 1, bg = 1, type = "p")
