d80b66545ba2f1ca4be43472788cbf46ad73b1f8
SallyFreanOwen <sally.frean@gmail.com>
2018-11-28 16:03:14 +1300

    checking claim subset matches rain subset

--- EQC-07-creating-hist-inputs.R
+++ EQC-07-creating-hist-inputs.R
@@ -5,8 +5,10 @@ claimPortfolioSpatialVCS <- mutate(claimPortfolioSpatialVCS,
 names(claimPortfolioSpatialVCS)[10] <- c("offsetRaw")
 claimPortfolioSpatialVCS$offset <- as.double(claimPortfolioSpatialVCS$offsetRaw)
 
+# Keep only claims from 1999 or 2000 
+claimPortfolioSpatialVCS <- filter(claimPortfolioSpatialVCS, lossDate <= "2000-12-01")
+claimPortfolioSpatialVCS <- filter(claimPortfolioSpatialVCS, lossDate >= "1999-01-01")
+
 # Keep if offset is fewer than 10 or greater than -10 
 claimPortfolioSpatialVCS <- filter(claimPortfolioSpatialVCS, offsetRaw <= 10)
 claimPortfolioSpatialVCS <- filter(claimPortfolioSpatialVCS, offsetRaw >= -10)
-
-
