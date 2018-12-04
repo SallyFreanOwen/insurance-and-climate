31e3f76882d8e6a36853412c7c99d697c61f5921
SallyFreanOwen <sally.frean@gmail.com>
2018-12-04 01:15:46 +0000

    use more data

--- EQC-07-creating-hist-inputs.R
+++ EQC-07-creating-hist-inputs.R
@@ -6,8 +6,8 @@ names(claimPortfolioSpatialVCS)[10] <- c("offsetRaw")
 claimPortfolioSpatialVCS$offset <- as.double(claimPortfolioSpatialVCS$offsetRaw)
 
 # Keep only claims from 1999 or 2000 
-claimPortfolioSpatialVCS <- filter(claimPortfolioSpatialVCS, lossDate >= "2000-01-01")
-claimPortfolioSpatialVCS <- filter(claimPortfolioSpatialVCS, lossDate <= "2004-12-01")
+# claimPortfolioSpatialVCS <- filter(claimPortfolioSpatialVCS, lossDate >= "2000-01-01")
+# claimPortfolioSpatialVCS <- filter(claimPortfolioSpatialVCS, lossDate <= "2004-12-01")
 
 # Keep if offset is fewer than 10 or greater than -10 
 claimPortfolioSpatialVCS <- filter(claimPortfolioSpatialVCS, offsetRaw <= 10)
