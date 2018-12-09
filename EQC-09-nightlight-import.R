eb02058586e945056d16fde0a4068e81a5699c4a
Sally Owen <sally.owen@motu.org.nz>
2018-12-10 11:41:10 +1300

    Experimenting with raster formats

--- EQC-09-nightlight-import.R
+++ EQC-09-nightlight-import.R
@@ -1,25 +1,54 @@
+
+
+# Downloading:
 download.file(
   "https://data.ngdc.noaa.gov/instruments/remote-sensing/passive/spectrometers-radiometers/imaging/viirs/dnb_composites/v10/201204/vcmcfg/SVDNB_npp_20120401-20120430_00N060E_vcmcfg_v10_c201605121456.tgz",
   destfile="Data/SVDNB_npp_20120401-20120430_00N060E_vcmcfg_v10_c201605121456.tgz"
 )
-
 download.file(
   "https://data.ngdc.noaa.gov/instruments/remote-sensing/passive/spectrometers-radiometers/imaging/viirs/dnb_composites/v10/201205/vcmcfg/SVDNB_npp_20120501-20120531_00N060E_vcmcfg_v10_c201605121458.tgz",
   destfile="Data/SVDNB_npp_20120501-20120531_00N060E_vcmcfg_v10_c201605121458.tgz"
 )
 
+# Unzipping raw downloads: 
 library(R.utils)
-gunzip("Data/SVDNB_npp_20120401-20120430_00N060E_vcmcfg_v10_c201605121456.tgz",destname="Data/S201204.tar")
-gunzip("Data/SVDNB_npp_20120501-20120531_00N060E_vcmcfg_v10_c201605121458.tgz",destname="Data/S201205.tar")
 
-untar("Data/S201204.tar",exdir="Data")
-untar("Data/S201205.tar",exdir="Data")
+gunzip("Data/SVDNB_npp_20120401-20120430_00N060E_vcmcfg_v10_c201605121456.tgz",
+       destname="Data/S201204.tar")
+gunzip("Data/SVDNB_npp_20120501-20120531_00N060E_vcmcfg_v10_c201605121458.tgz",
+       destname="Data/S201205.tar")
+untar("Data/S201204.tar", exdir="Data")
+untar("Data/S201205.tar", exdir="Data")
 
 # note tifs in . not Data/
-library(stars)
+
+##Obtain a list of TIF files, load in the first file in list
+tifs = list.files("Data", pattern = "\\.tif")
+rast1 <- raster(paste("Data", "/", tifs[1], sep=""))
+rast2 <- raster(paste("Data", "/", tifs[2], sep=""))
+rast3 <- raster(paste("Data", "/", tifs[3], sep=""))
+rast4 <- raster(paste("Data", "/", tifs[4], sep=""))
+
+# testing different aggregation options 
+stack1 <- stack(rast1, rast2, rast3, rast4)
+brick1 <- brick(rast1, rast2, rast3, rast4)
+
+##Specify WGS84 as the projection of the raster file
+wgs84 <- "+proj=longlat +datum=WGS84 +ellps=WGS84 +towgs84=0,0,0"
+projection(rast4) <- CRS(wgs84)
+
+library(raster)
+plot(rast2)
+
+
+#### Alternately - could use stars 
+
+#library(stars)
 #s201404 <- read_stars("Data/SVDNB_npp_20120401-20120430_00N060E_vcmcfg_v10_c201605121456.cf_cvg.tif")
 
-s <- read_stars(c("Data/SVDNB_npp_20120401-20120430_00N060E_vcmcfg_v10_c201605121456.cf_cvg.tif",
+#s <- read_stars(c("Data/SVDNB_npp_20120401-20120430_00N060E_vcmcfg_v10_c201605121456.cf_cvg.tif",
                   "Data/SVDNB_npp_20120501-20120531_00N060E_vcmcfg_v10_c201605121458.cf_cvg.tif"))
                         
-plot(s)
+#plot(s) #Error: cannot allocate vector of size 3.3 Gb
+
+
