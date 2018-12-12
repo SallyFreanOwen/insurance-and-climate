4906c9e2e8fe732fcf0626244343d869f5f92f79
SallyFreanOwen <sally.frean@gmail.com>
2018-12-12 03:25:58 +0000

    extract average TIF from NOAA tgzs

--- urls.sh
+++ urls.sh
@@ -1,2 +1,6 @@
 #!/bin/bash -x
+cd Data/
 wget -A "*00N060E*tgz" -i urls.txt -nd -np -r -S
+ls *00N060E*tgz | xargs -n 1 -P 7 tar --wildcards -x *.avg_rade9h.tif -f
+ls *00N060E*tgz | xargs rm
+cd ..
