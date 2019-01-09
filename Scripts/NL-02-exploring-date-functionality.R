d675c2eb570a9286395bed57cbde2e976a341a8a
Sally Owen <sally.owen@motu.org.nz>
2019-01-10 10:18:11 +1300

    adding examples of functionality

--- Scripts/NL-02-exploring-date-functionality.R
+++ Scripts/NL-02-exploring-date-functionality.R
@@ -28,3 +28,25 @@ nl201204 ####  should return:
 
 # unfortunately doesn't seem to have time or date information stored in the stars object created direct fro mthe .tif 
 
+### Stars functionality for time:
+
+# If we had read in, for example, a raster time series such as a netcdf with x, y, and time dimensions, 
+# stars would return an object that had a third "dimension", such as this example (below) from 
+# https://www.r-spatial.org/r/2017/11/23/stars1.html#reading-a-satellite-image, which has "time" with units and time steps
+
+# prec
+## stars object with 3 dimensions and 4 attributes
+## attribute(s), of first 1e+05 cells:
+##   p [(mm/day)]    sd [(mm/day)]       ek [%]       s [gauges per gridcell]
+##  Min.   :  0.00   Min.   : 0.00   Min.   :  1.02   Min.   :-99999         
+##  1st Qu.:  0.00   1st Qu.: 0.00   1st Qu.: 15.38   1st Qu.:-99999         
+##  Median :  0.10   Median : 0.16   Median : 24.70   Median :-99999         
+##  Mean   :  1.71   Mean   : 1.74   Mean   : 29.72   Mean   :-65915         
+##  3rd Qu.:  0.97   3rd Qu.: 1.14   3rd Qu.: 40.20   3rd Qu.:     0         
+##  Max.   :133.53   Max.   :73.51   Max.   :100.00   Max.   :   101         
+##  NA's   :66000    NA's   :66000   NA's   :66000                           
+## dimension(s):
+##      from  to     offset  delta  refsys point values
+## x       1 360       -180      1      NA    NA   NULL
+## y       1 180         90     -1      NA    NA   NULL
+## time    1 365 2013-01-01 1 days POSIXct    NA   NULL
