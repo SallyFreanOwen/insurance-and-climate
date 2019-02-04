00fc5b20b5a5ee27998abbf9132dd2ab0480910e
Sally Owen <sally.owen@motu.org.nz>
2019-02-04 16:04:12 +1300

    Added earlier code - regarding NIWA's historic weather events Database

---
+++ Archive/Extracting_Obs_Data.R
@@ -0,0 +1,38 @@
+# December 2017 
+
+# Installing ncdf4 packages:
+#install.packages("ncdf4")
+#install.packages("ncdf.tools")
+#install.packages("ncdf.tools")
+#install.packages("RNetCDF")
+#install.packages("chron")
+#install.packages("RColorBrewer")
+#install.packages("lattice")
+#install.packages("reshape2")
+#install.packages("dplyr")
+#install.packages("raster") # package for raster manipulation
+#install.packages("rgdal") # package for geospatial analysis
+#install.packages("ggplot2")
+#install.packages("RSAGA")
+#install.packages("GISTools")
+#install.packages("arrayhelpers")
+
+# Calling them all 
+library(ncdf.tools)
+library(ncdf4)
+library(ncdf4.helpers)
+library(RNetCDF)#Loading other helpful things: 
+library(chron)
+library(RColorBrewer)
+library(lattice)
+library(reshape2)
+library(dplyr) # More helpful things:
+library(raster) # package for raster manipulation
+library(rgdal) # package for geospatial analysis
+library(RSAGA)
+library(ggplot2) # package for plotting
+library(GISTools)
+library(arrayhelpers)
+
+# Setting working directory 
+setwd("R:/Agriculture/Deep South Challenge 2017/Netcdf_observationsdata")
