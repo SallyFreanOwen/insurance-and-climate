0ab1dcfc02e3fef106eefe6c1f506bb9aff44ed1
SallyFreanOwen <sally.frean@gmail.com>
2019-02-12 08:53:09 +1300

    WIP

--- Scripts/WIP.R
+++ Scripts/WIP.R
@@ -1,4 +1,7 @@
-# Working on EQC summary stats 
+# Working on EQC 
+
+library(dplyr)
+library(reshape2)
 
 rm(claim0004, claim0508, claim0912, claim1316, 
    vcsn0004, vcsn0508, vcsn0912, vcsn1316, 
@@ -8,3 +11,8 @@ rm(claim0004, claim0508, claim0912, claim1316,
    claimPortfolioSpatialVCS0912, claimPortfolioSpatialVCS1316
    )
 
+# Quick eyeball, getting up to speed 
+sumary(claimPortfolioSpatial)
+
+claimPortfolioSpatial <- mutate(claimPortfolioSpatial, eventMonth=month(eventDate))
+claimPortfolioSpatial <- mutate(claimPortfolioSpatial, eventYear=year(eventDate))
