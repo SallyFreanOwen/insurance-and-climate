94b753975d80960334badfbed7e0ca7c09f06fc8
SallyFreanOwen <sally.frean@gmail.com>
2019-02-12 19:35:52 +1300

    WIP

--- Scripts/WIP.R
+++ Scripts/WIP.R
@@ -2,6 +2,7 @@
 
 library(dplyr)
 library(reshape2)
+library(lubridate)
 
 rm(claim0004, claim0508, claim0912, claim1316, 
    vcsn0004, vcsn0508, vcsn0912, vcsn1316, 
@@ -12,7 +13,18 @@ rm(claim0004, claim0508, claim0912, claim1316,
    )
 
 # Quick eyeball, getting up to speed 
-sumary(claimPortfolioSpatial)
+summary(claimPortfolioSpatial)
 
 claimPortfolioSpatial <- mutate(claimPortfolioSpatial, eventMonth=month(eventDate))
 claimPortfolioSpatial <- mutate(claimPortfolioSpatial, eventYear=year(eventDate))
+
+# Recreating histograms from first paper 
+hist(claimPortfolioSpatial$eventYear)
+hist(claimPortfolioSpatial$eventMonth)
+
+#Subsetting for speed while working out process:
+claimPortfolioSpatialTestSub <- filter(claimPortfolioSpatial, eventYear == 2016)
+vcsnTestSub <- filter(vcsn, vcsn$vcsnDay > "2015-12-31")
+
+claimPortfolioSpatialTestVCSN <- merge(claimPortfolioSpatialTestSub, vcsnTestSub, by = c("vcsnLongitude", "vcsnLatitude"))
+
