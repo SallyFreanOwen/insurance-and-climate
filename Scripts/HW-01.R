def17499e2e2b5f5cf354a7d811aabce360fefa3
SallyFreanOwen <sally.frean@gmail.com>
2019-03-11 18:26:41 +1300

    finished cleaning (dates/naming conventions etc)

--- Scripts/HW-01.R
+++ Scripts/HW-01.R
@@ -1,11 +1,24 @@
 # HW - 02 Initial investigation HWEC 
 
-histWeather_raw <- read.csv("Data/HistoricEventsCatalog.csv")
+histWeatherRaw <- read.csv("Data/HistoricEventsCatalog.csv", stringsAsFactors = FALSE)
 
 library(dplyr)
 library(lubridate)
 library(reshape2)
 
-claims <- mutate(claims, eventMonth=month(eventDate))
-claims <- mutate(claims, eventYear=year(eventDate))
+histWeather <- histWeatherRaw
+
+# Formatting names 
+first.letter  <- tolower(substring(names(histWeatherRaw),1, 1))
+other.letters <- substring(names(histWeatherRaw), 2)
+newnames      <- paste(first.letter, other.letters, sep="")
+names(histWeather) <- newnames
+
+# sorting out the ID 
+histWeather$identifier <- as.numeric(histWeather$identifier)
+
+# sorting out dates:
+histWeather$startDate <- as.Date(histWeather$startDate, format = "%Y-%m-%d")
+
+rm(histWeatherRaw)
 
