7b5d0640db4b1806fc1390f4323276897440002d
SallyFreanOwen <sally.frean@gmail.com>
2019-03-17 13:15:06 +1300

    WIP

--- Scripts/EQC-03-analysis-sum-stats.R
+++ Scripts/EQC-03-analysis-sum-stats.R
@@ -1,23 +1,28 @@
 # EQC claim vis  
 
-# Summary statistics 
+# Summary statistics - individually:
 library(pastecs)
 options(digits=3)
 stat.desc(claims[,sapply(claims,is.numeric)], basic=F)
 
-library(stargazer)
-stargazer(claims)
+#library(stargazer)
+#stargazer(claims)
 
 # EQC - visualise the eqc property info (portfolio)
 
 # assume have run scripts 1 and 2 
-par(mar=c(1,1,1,1)) # (making margins large enough) 
-plot(st_geometry(portfolios), main="All Portfolios", pch = 1, cex = 0.1, col = 1, bg = 1, type = "p")
+#par(mar=c(1,1,1,1)) # (making margins large enough) 
+#plot(st_geometry(portfolios), main="All Portfolios", pch = 1, cex = 0.1, col = 1, bg = 1, type = "p")
 
 # Summary statistics 
 library(pastecs)
 options(digits=3)
 stat.desc(portfolios[,sapply(portfolios,is.numeric)], basic=F)
 
-library(stargazer)
-stargazer(portfolios)
+#library(stargazer)
+#stargazer(portfolios)
+
+library(pastecs)
+options(digits=3)
+stat.desc(claimPortfolioVcsnFull[,sapply(claimPortfolioVcsnFull,is.numeric)], basic=F)
+
