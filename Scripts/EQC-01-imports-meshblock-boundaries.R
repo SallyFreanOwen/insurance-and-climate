73c01c0c2256072bfb76905817002cda148238c6
SallyFreanOwen <sally.frean@gmail.com>
2019-03-17 18:44:20 +1300

    modified for area units

--- Scripts/EQC-01-imports-meshblock-boundaries.R
+++ Scripts/EQC-01-imports-meshblock-boundaries.R
@@ -3,21 +3,21 @@
 library(sf)
 library(dplyr)
 
-shapefilename <- "Data/Meshblocks/ESRI_Census_Based_2013_NZTM/ESRI shapefile Output/2013 Digital Boundaries Generalised Full/MB2013_GV_Full.shp"
+shapefilename <- "Data/Meshblocks/ESRI_Census_Based_2013_NZTM/ESRI shapefile Output/2013 Digital Boundaries Generalised Full/AU2013_GV_Full.shp"
 #"Data/Meshblocks/MB2016_GV_Clipped.shp"
-meshblockImportRaw <- st_read(shapefilename)
+auImportRaw <- st_read(shapefilename)
 #check it worked: 
-meshblockImportRaw
+auImportRaw
 #plot(meshblockImportRaw)
 
-mbBoundaries2013 <- select(meshblockImportRaw, MB2013) 
-mbBoundaries2013 <- st_cast(mbBoundaries2013, "POLYGON")
-mbBoundaries2013 <- st_transform(st_geometry(mbBoundaries2013), crs = 4326)
+auBoundaries2013 <- select(auImportRaw, AU2013) 
+auBoundaries2013 <- st_cast(auBoundaries2013, "POLYGON")
+auBoundaries2013 <- st_transform(st_geometry(auBoundaries2013), crs = 4326)
 
 portfolios_sf <- st_geometry(portfolios)
 portfolios_wgs84 <- st_transform(portfolios_sf, crs = 4326)
 
 st_crs(portfolios_wgs84)
-st_crs(mbBoundaries2013)
+st_crs(auBoundaries2013)
 
-portfolioMbID <- st_join(st_buffer(portfolios_wgs84, dist = 5e4), mbBoundaries2013)
+portfolioMbID <- st_join(st_buffer(portfolios_wgs84, dist = 5e4), auBoundaries2013)
