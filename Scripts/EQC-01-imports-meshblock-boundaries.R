f8e0af4c632eac45f19a7acfbba164d392d6b1e0
SallyFreanOwen <sally.frean@gmail.com>
2019-03-17 16:53:56 +1300

    WIP

--- Scripts/EQC-01-imports-meshblock-boundaries.R
+++ Scripts/EQC-01-imports-meshblock-boundaries.R
@@ -1,24 +1,23 @@
 # Importing meshblock boundaries 
 
 library(sf)
+library(dplyr)
 
 shapefilename <- "Data/Meshblocks/ESRI_Census_Based_2013_NZTM/ESRI shapefile Output/2013 Digital Boundaries Generalised Full/MB2013_GV_Full.shp"
-  #"Data/Meshblocks/MB2016_GV_Clipped.shp"
+#"Data/Meshblocks/MB2016_GV_Clipped.shp"
 meshblockImportRaw <- st_read(shapefilename)
 #check it worked: 
 meshblockImportRaw
 #plot(meshblockImportRaw)
 
-meshblockImportRaw %>% 
-st_transform(crs = "+proj=moll +datum=WGS84")
+mbBoundaries2013 <- select(meshblockImportRaw, MB2013) 
+mbBoundaries2013 <- st_cast(mbBoundaries2013, "POLYGON")
+mbBoundaries2013 <- st_transform(st_geometry(mbBoundaries2013), crs = 4326)
 
-meshblockImportTest2 <- st_cast(meshblockImportRaw$geometry, "POLYGON")
-meshblockImportTest2
+portfolios_sf <- st_geometry(portfolios)
+portfolios_wgs84 <- st_transform(portfolios_sf, crs = 4326)
 
-#plot(meshblockImportRaw$mb2016)
+st_crs(portfolios_wgs84)
+st_crs(mbBoundaries2013)
 
-rm(shapefilename) 
-
-
-
-
+portfolioMbID <- st_join(st_buffer(portfolios_wgs84, dist = 5e4), mbBoundaries2013)
