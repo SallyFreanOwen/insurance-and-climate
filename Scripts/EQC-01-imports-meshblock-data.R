b5572bdff489db2a7aa475bd5c726746fdec3853
SallyFreanOwen <sally.frean@gmail.com>
2019-11-06 13:02:33 +1300

    tidying as wrapping up

--- Scripts/EQC-01-imports-meshblock-data.R
+++ Scripts/EQC-01-imports-meshblock-data.R
@@ -1,5 +1,5 @@
 
-# Census data (cleaned from thesis project) importing  
+# Census data (cleaned from thesis project) inporting  
 
 #setwd("~/EQC-climate-change-part-two")
 
@@ -16,12 +16,10 @@ rm(meshblockRaw)
 
 #import meshblock-level census vars 
 censusRaw <- read.csv("Data/thesis_census_data.csv", stringsAsFactors = FALSE)
-mbCensusStats <- censusRaw[,c("mb2013", "Mean_Num_HH_Members_2013", "Median_HH_Income_2013", "Prop_Dwelling_Not_Owned_2013")]
+mbCensusStats <- censusRaw[,c("mb2013", "Mean_Num_HH_Members_2013", "Median_HH_Income_2013", "Median_Weekly_Rent_Paid_2013", "Prop_Dwelling_Not_Owned_2013")]
 
 rm(censusRaw)
 
-names(mbCensusStats) <- c("mb2013", "meanNumHHMembers", "medianHHIncome", "propDwellingNotOwned")  # first name issue: 
-                    
 mbStats <- merge(mbCensusStats, portfoliodMbID, by = "mb2013")
 
 rm(mbCensusStats)
